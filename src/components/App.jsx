f1076cf7f2901cfcb9b4a65f18dcb58b3eb253c2
Charls Cabangon <charlsmercabangon@gmail.com>
2025-10-18 21:41:49 +0800

    feat: set up routing in App and main components

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,6 +1,20 @@
+import { Routes, Route } from 'react-router-dom';
+
+import Header from './Header';
+import Footer from './Footer';
+
 import HomePage from '@/pages/HomePage';
 import ShopPage from '@/pages/ShopPage';
 
 export default function App() {
-  return <ShopPage />;
+  return (
+    <>
+      <Header />
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/shop" element={<ShopPage />} />
+      </Routes>
+      <Footer />
+    </>
+  );
 }
--- src/main.jsx
+++ src/main.jsx
@@ -1,14 +1,16 @@
-import { StrictMode } from 'react';
+import React from 'react';
 import { createRoot } from 'react-dom/client';
+import { BrowserRouter } from 'react-router-dom';
 
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import { Analytics } from '@vercel/analytics/react';
 import '@/styles/index.css';
 
 import App from '@/components/App.jsx';
 
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>
+  <React.StrictMode>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </React.StrictMode>
 );
