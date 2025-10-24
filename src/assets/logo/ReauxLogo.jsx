964c74bbea7fbca2b5e2d41b26b4c5ee37d62450
Charls Cabangon <charlsmercabangon@gmail.com>
2025-10-24 22:01:54 +0800

    feat: add Reaux logo component

---
+++ src/assets/logo/ReauxLogo.jsx
@@ -0,0 +1,26 @@
+import PropTypes from 'prop-types';
+
+export default function ReauxLogo({ color = 'brand-black', size = 15, hasHover = false }) {
+  const logoSrcMap = {
+    'brand-black': '/assets/logo/reaux-wordmark-brand-black.svg',
+    white: '/assets/logo/reaux-wordmark-white.svg',
+    black: '/assets/logo/reaux-wordmark-black.svg',
+  };
+
+  const src = logoSrcMap[color] || logoSrcMap['brand-black'];
+
+  return (
+    <img
+      src={src}
+      alt="Reaux logo"
+      style={{ width: `${size}vw` }}
+      className={`max-w-[300px] cursor-pointer transition-transform duration-300 ease-in-out ${hasHover ? 'hover:scale-105' : ''}`}
+    />
+  );
+}
+
+ReauxLogo.propTypes = {
+  color: PropTypes.oneOf(['white', 'black', 'brand-black']),
+  size: PropTypes.number,
+  hasHover: PropTypes.bool,
+};
