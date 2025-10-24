82aeea32308348a16ea95f41db3dded60f85762c
Charls Cabangon <charlsmercabangon@gmail.com>
2025-10-24 22:04:54 +0800

    feat: add video option to Banner component

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -2,24 +2,34 @@ import PropTypes from 'prop-types';
 
 import Heading from './Heading';
 
-export default function Banner({ img, imgClassName, hasCTA = true }) {
+export default function Banner({ banner, imgClassName, hasCTA = true }) {
+  const isVideo = banner?.type === 'video';
   return (
-    <section className="relative flex h-[75vh] w-full justify-center">
+    <section className="relative flex h-[75vh] w-full justify-center overflow-hidden">
       <div className="relative h-full w-full">
-        <img
-          src={img.image}
-          alt={img.alt}
-          className={`absolute inset-0 h-full w-full object-cover ${imgClassName}`}
-        />
+        {isVideo ? (
+          <video
+            src={banner.src.video}
+            className={`absolute inset-0 h-full w-full object-cover ${imgClassName}`}
+            autoPlay
+            loop
+            muted
+            playsInline
+          />
+        ) : (
+          <img
+            src={banner.src.image}
+            alt={banner.src.alt}
+            className={`absolute inset-0 h-full w-full object-cover ${imgClassName}`}
+          />
+        )}
         <div className="absolute inset-0 bg-[#000]/60"></div>
       </div>
-      <div className="absolute bottom-16 z-10 w-2/3 text-center text-off-white">
-        <Heading heading={img.title} />
+      <div className="absolute bottom-10 z-10 flex w-2/3 flex-col items-center text-center text-off-white">
+        <Heading heading={banner.src.title} />
         <p className="mb-3 font-light">
           Lorem, ipsum dolor sit amet consectetur adipisicing elit. Reprehenderit soluta enim dicta
-          atque iste, illo, et delectus, quibusdam quos suscipit sequi quasi eum. Cupiditate dolore
-          recusandae consequatur natus exercitationem beatae ipsa architecto earum repellendus!
-          Suscipit.
+          atque iste, illo, et delectus, quibusdam quos.
         </p>
         {hasCTA && (
           <a href="" className="text-off-white-muted">
@@ -32,11 +42,13 @@ export default function Banner({ img, imgClassName, hasCTA = true }) {
 }
 
 Banner.propTypes = {
-  img: PropTypes.shape({
-    id: PropTypes.string,
-    title: PropTypes.string.isRequired,
-    image: PropTypes.string.isRequired,
+  banner: PropTypes.shape({
+    type: PropTypes.oneOf(['image', 'video']).isRequired,
+    src: PropTypes.string.isRequired,
     alt: PropTypes.string,
+    title: PropTypes.string,
+    description: PropTypes.string,
+    link: PropTypes.string,
   }).isRequired,
   imgClassName: PropTypes.string,
   hasCTA: PropTypes.bool,
