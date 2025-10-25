b3c3cb75c02a64d2315830c8f79eb1fd379787a2
Charls Cabangon <charlsmercabangon@gmail.com>
2025-10-25 21:53:18 +0800

    feat: finish About page UI

--- src/pages/AboutPage.jsx
+++ src/pages/AboutPage.jsx
@@ -1,22 +1,39 @@
 import Feature from '@/components/Feature';
 import Heading from '@/components/Heading';
+import PressMarquee from '@/components/Marquee';
+import CTA from '@/components/CTA';
+import { PrimaryBtn } from '@/components/controls/Button';
 
 import { getAsset } from '@/data/assetData';
 
 export default function AboutPage() {
   return (
     <main className="mt-56 flex flex-col">
-      <div className="text-center">
+      <div className="flex flex-col items-center gap-12 text-center">
         <h2>THE STORY OF REAUX</h2>
+        <div className="max-w-[60ch] space-y-3">
+          <p>
+            Reaux was born from a love for clean lines and quiet detail. Each piece is crafted with
+            purpose—modern, refined, and timeless.
+          </p>
+          <em className="font-light">Rendered perfectly.</em>
+        </div>
       </div>
       <hr />
-      <Feature img={getAsset('ariana-swarovski-capsule-collection')} hasContent={false}>
-        <Heading heading="About us" />
-      </Feature>
+      <Feature feature={getAsset('vision-rendered')} />
       <hr />
-      <Feature img={getAsset('fine-design')} isLeft={false} />
+      <Feature feature={getAsset('fine-design')} isLeft={false} />
+      <PressMarquee />
+      <Feature feature={getAsset('reaux-worldwide')} headingGap={true} />
       <hr />
-      <Feature img={getAsset('reactive-framework')} />
+      <CTA>
+        <h3>Luxury has its circle. You’re invited.</h3>
+        <p>
+          Joining Reaux Club means first access to limited collections, private previews, and
+          personal styling insights — all crafted for those who see luxury as an art form.
+        </p>
+        <PrimaryBtn name="Join the Reaux Club" />
+      </CTA>
     </main>
   );
 }
