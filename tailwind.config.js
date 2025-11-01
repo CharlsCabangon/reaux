afe3a341b36dd9d1a66611e4d8f2c3ea57ce4c7d
Charls Cabangon <charlsmercabangon@gmail.com>
2025-11-01 22:32:57 +0800

    chore: add xs as an additional screen breakpoint

--- tailwind.config.js
+++ tailwind.config.js
@@ -17,6 +17,14 @@ module.exports = {
         sourceSerif: ['"Source Serif 4"', 'serif'],
         roboto: ['Roboto', 'sans-serif'],
       },
+      screens: {
+        xs: '475px',
+        sm: '640px',
+        md: '768px',
+        lg: '1024px',
+        xl: '1280px',
+        '2xl': '1536px',
+      },
       maxWidth: {
         '60ch': '60ch',
         '75ch': '75ch',
