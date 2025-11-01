bb45ebd21ff300278d92fd64249bd83a68772de2
Charls Cabangon <charlsmercabangon@gmail.com>
2025-11-01 22:34:11 +0800

    chore: put footer links in a separate file

---
+++ src/data/footerData.js
@@ -0,0 +1,21 @@
+export const socials = {
+  github: 'https://github.com/CharlsCabangon',
+};
+
+export const footerSections = [
+  {
+    id: 'company',
+    title: 'OUR COMPANY',
+    links: ['About Reaux', 'Careers', 'Shops', 'Credits', 'Sitemap'],
+  },
+  {
+    id: 'customer',
+    title: 'CUSTOMER CARE',
+    links: ['Contact Us', 'FAQ', 'Track Your Order', 'Book an Appointment', 'Accessibility'],
+  },
+  {
+    id: 'legal',
+    title: 'LEGAL AREA',
+    links: ['Terms of Use', 'Terms & Conditions', 'Privacy Policy', 'Imprint', 'Cookie Consent'],
+  },
+];
